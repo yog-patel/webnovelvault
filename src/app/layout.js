01cc70dd43527395883fdcbace74d97cdff9d3d5
Yog Patel <yg371732@dal.ca>
2025-05-12 15:07:59 -0300

    fav-icon added- Noticed some funny business on the home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/big_fav_icon.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <Providers>
           <Layout>
