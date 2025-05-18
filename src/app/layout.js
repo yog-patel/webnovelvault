4a6b9c70e82d069f23df34833a42956dc52f517b
Yog Patel <yg371732@dal.ca>
2025-05-18 16:19:39 -0300

    head removed from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" type="image/ico" />
-      </head>
       <body className={inter.className}>
         <Providers>
           <Layout>
